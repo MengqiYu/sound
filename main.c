5c62064c4caadf491a6159f7ea0a1ad13f7f7950
MengqiYu <37363737+MengqiYu@users.noreply.github.com>
2018-04-03 11:11:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sound.h"
-
+#include <signal.h>
 int main(void){
 	int ret;
 	while(1){
 		//record 1 sec of sound into test.wav
 		ret = system("arecord -q -r16000 -c1 -f S16_LE -d1 test.wav");
-		if(WIFSIGNALED(ret) && (WTERTMSIC(ret)==SIGINT))break;
+		if(WIFSIGNALED(ret) && (WTERMSIG(ret)==SIGINT))break;
 		//open the wav file and read samples
 		//display necessary information (duration,wav header etc)
 		displayWAVheader("test.wav");
