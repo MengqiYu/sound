dcc6f5dc4382becd2c3c18b63744efdea0262c78
2 <37363737+MengqiYu@users.noreply.github.com>
2018-04-18 01:18:12 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
+// this file contains the main function of the program
 #include <stdio.h>
 #include <stdlib.h>
 #include "sound.h"
 #include <signal.h>
+#include "screen.h"
 int main(void){
 	int ret;
-	while(1){
+	while(1){ //loop runs forever
 		//record 1 sec of sound into test.wav
 		ret = system("arecord -q -r16000 -c1 -f S16_LE -d1 test.wav");
 		if(WIFSIGNALED(ret) && (WTERMSIG(ret)==SIGINT))break;
@@ -17,4 +19,5 @@ int main(void){
 		//send fast dBs to web (php program on www.cc.puv.fi)
 
 	}
+	return 0;
 }
