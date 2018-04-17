9c5a0300bb61e8a2063dd8aa39ecd14193ffdf49
2 <37363737+MengqiYu@users.noreply.github.com>
2018-04-18 01:12:40 +0800

    Update comm.c

--- comm.c
+++ comm.c
@@ -8,17 +8,17 @@
 //corresponding to 2000 samples or 125ms of sound
 void sendToServer(double r80[]){
     double r8[8], sum;
-    int i,j;
+    int i,j;   // loop counters
     CURL *curl;
     CURLcode res;
     char post[1000];
-    for(i=0; i<8; i++){
-        sum = 0;
-        for(j=0; j<10; j++){
+    for(i=0; i<8; i++){  // outer loop repeats 8 times for 8-pieces of result
+        sum = 0;  
+        for(j=0; j<10; j++){   // inner loop repeats 10 times to accumulate
             sum += r80[j+i*10]*r80[j+i*10]*200;
         }
         r8[i] = sqrt(sum/2000);
-    }
+    }   // end of for[i]
     //prepare post data
     sprintf(post, "data=%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%.2f",
             r8[0],r8[1],r8[2],r8[3],r8[4],r8[5],r8[6],r8[7]);
@@ -33,7 +33,7 @@ void sendToServer(double r80[]){
               fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
         curl_easy_cleanup(curl);
-    }
+    }   //end of if
     curl_global_cleanup();
 }
 
