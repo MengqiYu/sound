8cc8212f6d547bff81e1e714bd8829a0e60c5234
2 <37363737+MengqiYu@users.noreply.github.com>
2018-04-18 01:21:42 +0800

    Update screen.c

--- screen.c
+++ screen.c
@@ -6,7 +6,7 @@
 void clearScreen(void){
 	printf("%c[2J",ESC);
 	fflush(stdout);
-}
+}       //This function uses VT100 escape sequence \ESC[2J to make the whole screen empty
 
 void gotoxy(int row, int col){
 	printf("%c[%d;%dH",ESC,row,col);
@@ -22,7 +22,7 @@ void bar(int col,double dB){
 	int i;
 	for(i=0; i<dB/4; i++){
 		gotoxy(25-i, col+1);
-#ifndef UNICODE
+#ifndef UNICODE   // if unicode is not enabled
 		printf("%c", '*');
 #else
 		if(i<60/4) setColor(WHITE);
